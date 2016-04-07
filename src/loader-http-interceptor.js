517a3849cd3033d2d82b27e4b42a15de26219ce9
eubw <dstepbw@gmail.com>
2016-04-07 01:06:43 -0300

    add css

--- src/loader-http-interceptor.js
+++ src/loader-http-interceptor.js
@@ -1,7 +1,14 @@
+/**
+* Basic loading for requisitions in angulars apps
+* in construction :)
+* Thanks for the loading css @brunjo - http://codepen.io/brunjo/pen/vELmaP   
+*/
 (function(){
 'use strict';
   angular
   .module('LoadindFeedBackInterceptorModule',[])
+  .directive('loadingShow', loadingShow)
+  .animation('.notification', animationConfig)
   .config(config);
 
   config.$inject = ['$provide', '$httpProvider'];
@@ -39,7 +46,7 @@
       
       function openLoading(configUrl){
         requestList.push(configUrl);
-        $rootScope.$broadcast('NewRequestEvent');
+        $rootScope.$broadcast('OpenLoadindEvent');
       };
       
       function closeLoading(configUrl){
@@ -52,4 +59,46 @@
     
     $httpProvider.interceptors.push('LoadindFeedBackInterceptor');
   };
+  
+  loadingShow.$inject = ['$rootScope'];
+  function loadingShow($rootScope){
+    var directive = {
+      restrict: 'E',
+      scope:{
+        loadingMessage: '@',
+        barColor: '@'
+      },
+      template: '<div data-ng-if="ativeLoading" class="loading-bar"><b>{{loadingMessage}}</b><i class="signal-in-line"></i></div>',
+      link: link
+    };
+    return directive;
+    
+    function link(scope, element, attrs) {
+      scope.ativeLoading = false;
+      $rootScope.$on('OpenLoadindEvent', showThis);
+      $rootScope.$on('CloseLoadingEvent', hideThis);
+      
+      function showThis(){
+        scope.ativeLoading = true;
+      };
+      
+      function hideThis(){
+        scope.ativeLoading = false;
+      };
+    };
+  };
+  
+  function animationConfig() {
+    return {
+      enter: function(element, done) {
+        console.log('>>>> enter');
+        element.hide().slideDown()
+        return function(cancelled) {};
+      },
+      leave: function(element, done) { 
+        element.slideUp();
+      },
+    };
+  };
+
 }());
